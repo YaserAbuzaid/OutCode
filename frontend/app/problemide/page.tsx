02a8b75a5a607bcd6ff64ba96b687ae631f099ca
Yasser Abouzaid <ibhvuy@gmail.com>
2026-04-22 12:03:44 +0300

    js added the ide, i feel like a genuis

--- frontend/app/problemide/page.tsx
+++ frontend/app/problemide/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import React from 'react'
-
+import Editor from '@monaco-editor/react';
 const page = () => {
+  const [language, setLanguage] = React.useState("javascript");
   return (
-    <div>
-      new page, hell yaeh
+    <div className='rounded-xl overflow-hidden border border-gray-800 shadow-lg'>
+      <Editor width="50vw" height="80vh" language={language} defaultValue="// goodluck" theme="vs-dark" className='m-2' />
+      <button onClick={() => setLanguage("csharp")} className='btn btn-active'>C#</button>
+      <button onClick={() => setLanguage("cpp")} className='btn btn-active'>C++</button>
+      <button onClick={() => setLanguage("java")} className='btn btn-active'>JAVA</button>
+      <button onClick={() => setLanguage("python")} className='btn btn-active'>PYTHON</button>
+      <button onClick={() => setLanguage("r")} className='btn btn-active'>R</button>
+      <button onClick={() => setLanguage("javascript")} className='btn btn-active'>JavaScript</button>
+
     </div>
   )
 }
